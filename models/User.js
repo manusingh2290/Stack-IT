690d4ce88b8dfba8878e8ae3466894d7a33c0351
Manendra Singh <singhdaleep8642@gmail.com>
2026-02-08 03:56:37 +0530

    Add avatar, bio, and reputation fields to User schema

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+//models/User.js:
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
@@ -16,7 +17,22 @@ const UserSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-  }
+  },
+
+  // ✅ REQUIRED FOR PROFILE
+  avatar: {
+    type: String,
+    default: "/uploads/default-avatar.png"
+  },
+  bio: {
+    type: String,
+    default: ""
+  },
+  reputation: {
+  type: Number,
+  default: 0
+}
+
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);
